d5ca892bd3f3a6c0148b093ce55fba2b946eb448
maralosil <maralosil@49ee3937-5266-4f4a-e7b9-40f6dfc295be>
2011-06-12 13:07:42 +0000

    added new card exceptions messages

---  scard --username maralosil@gmail.com/scardexception.cpp
+++  scard --username maralosil@gmail.com/scardexception.cpp
@@ -111,6 +111,99 @@ char* SCardException:: getMessage() {
 		case SCARD_E_NO_RESRC_MNGR_RUNNING:
 			return "The Smart card resource manager is not running.";
 
+		case ERROR_BROKEN_PIPE:
+			return "The client attempted a smart card operation in a remote session, such as a client session running on a terminal server, and the operating system in use does not support smart card redirection.";
+
+		case SCARD_E_BAD_SEEK:
+			return	"An error occurred in setting the smart card file object pointer.";
+
+		case SCARD_E_CANCELLED:
+			return "The action was canceled by an SCardCancel request.";
+
+		case SCARD_E_CANT_DISPOSE:
+			return "The system could not dispose of the media in the requested manner.";
+
+		case SCARD_E_CERTIFICATE_UNAVAILABLE:
+			return "The requested certificate could not be obtained.";
+
+		case SCARD_E_COMM_DATA_LOST:
+			return "A communications error with the smart card has been detected.";
+
+		case SCARD_E_DIR_NOT_FOUND:
+			return "The specified directory does not exist in the smart card.";
+
+		case SCARD_E_DUPLICATE_READER:
+			return "The reader driver did not produce a unique reader name.";
+
+		case SCARD_E_FILE_NOT_FOUND:
+			return "The specified file does not exist in the smart card.";
+
+		case SCARD_E_ICC_CREATEORDER:
+			return "The requested order of object creation is not supported.";
+
+		case SCARD_E_ICC_INSTALLATION:
+			return "No primary provider can be found for the smart card.";
+
+		case SCARD_E_INVALID_CHV:
+			return "The supplied PIN is incorrect.";
+
+		case SCARD_E_INVALID_PARAMETER:
+			return "One or more of the supplied parameters could not be properly interpreted.";
+
+		case SCARD_E_INVALID_VALUE:
+			return "One or more of the supplied parameter values could not be properly interpreted.";
+
+		case SCARD_E_NO_ACCESS:
+			return "Access is denied to the file.";
+
+		case SCARD_E_NO_DIR:
+			return "The supplied path does not represent a smart card directory.";
+
+		case SCARD_E_NO_FILE:
+			return "The supplied path does not represent a smart card file.";
+
+		case SCARD_E_NO_MEMORY:
+			return "Not enough memory available to complete this command.";
+
+		case SCARD_E_NO_SUCH_CERTIFICATE:
+			return "The requested certificate does not exist.";
+
+		case SCARD_E_NOT_TRANSACTED:
+			return "An attempt was made to end a nonexistent transaction.";
+
+		case SCARD_E_UNEXPECTED:
+			return "An unexpected card error has occurred.";
+
+		case SCARD_E_UNKNOWN_READER:
+			return "The specified reader name is not recognized.";
+
+		case SCARD_E_UNKNOWN_RES_MNG:
+			return "An unrecognized error code was returned.";
+
+		case SCARD_E_UNSUPPORTED_FEATURE:
+			return "This smart card does not support the requested feature.";
+
+		case SCARD_E_WRITE_TOO_MANY:
+			return "An attempt was made to write more data than would fit in the target object.";
+
+		case SCARD_S_SUCCESS:
+			return "No error was encountered.";
+
+		case SCARD_W_CANCELLED_BY_USER:
+			return "The action was canceled by the user.";
+
+		case SCARD_W_CHV_BLOCKED:
+			return "The card cannot be accessed because the maximum number of PIN entry attempts has been reached.";
+
+		case SCARD_W_EOF:
+			return "The end of the smart card file has been reached.";
+
+		case SCARD_W_SECURITY_VIOLATION:
+			return "Access was denied because of a security violation.";
+
+		case SCARD_W_WRONG_CHV:
+			return "The card cannot be accessed because the wrong PIN was presented.";
+			
 		default:
 			return "Unknown error.";
 
